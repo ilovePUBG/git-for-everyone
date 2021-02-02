ca4b5e11a4c5beee82dbac447d4da348ba9630b9
_PeterParker <jkshin0903@gmail.com>
2021-02-02 11:20:27 +0900

    lets go!

--- index.js
+++
@@ -1,6 +0,0 @@
-const names = ["nico", "lynn", "flynn"];
-
-// const hearts = names.map(item => {return item + "❤";});
-const hearts = names.map((item, index) => item + "❤" + index);
-
-console.log(hearts);
